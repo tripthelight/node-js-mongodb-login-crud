aa4190fdbe3810624e177738c930476e57ff1d5c
sin kim <bye_ok@naver.com>
2022-08-10 13:47:40 +0900

    [220810 - WORKING]

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 // Initializations
@@ -7,6 +8,13 @@ const app = express();
 // Settings
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.urlencoded({extended: false}));
@@ -15,8 +23,8 @@ app.use(express.urlencoded({extended: false}));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('hello world');
-})
+    res.render('index')
+});
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
