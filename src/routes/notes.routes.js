5e6d899327147faea84097a8d77d45b6c520731c
sin kim <bye_ok@naver.com>
2022-08-10 14:42:36 +0900

    [220810 - WORKING]

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const router = Router();
+
+const {
+    renderNoteForm,
+    createNewNote,
+    renderNotes,
+    renderEditForm,
+    updateNote,
+    deleteNote
+} = require('../controllers/notes.controllers');
+
+// New note
+router.get('/notes/add', renderNoteForm);
+router.post('/notes/add', createNewNote);
+
+// Get All note
+router.get('/notes', renderNotes);
+
+// Edit Notes
+router.get('/notes/edit/:id', renderEditForm);
+router.put('/notes/edit/:id', updateNote);
+
+// Delete Notes
+router.delete('/notes/delete/:id', deleteNote);
+
+module.exports = router;
